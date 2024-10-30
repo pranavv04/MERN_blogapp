ab817d56c990ce284a145e18ad2c1f2952f0e904
Pranav Dhangar <113121206+pranavv04@users.noreply.github.com>
2024-10-30 17:54:07 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,7 @@ const handleSubmit = async(e)=>{
   e.preventDefault();
 
   try {
-    const response = await fetch('http://localhost:2000/user/login',{
+    const response = await fetch('https://mern-blogapp-e6sj.onrender.com/user/login',{
       method:'POST',
       headers:{
         'Content-Type' : 'application/json',
